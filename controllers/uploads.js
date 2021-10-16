e05b7e83baf1621ae11653ecc136bd82ec95183c
Andres2D <andres2d1997.1206@gmail.com>
2021-10-15 22:08:34 -0500

    Add default image

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -81,7 +81,66 @@ const updateImage = async(req, res = response) => {
     }
 }
 
+const showImage = async(req, res = response) => {
+    try {
+        const {id, collection} = req.params;
+
+        let model;
+        switch(collection) {
+            case 'users':
+                model = await User.findById(id);
+                if(!model) {
+                    return res.status(400).json({
+                        ok: false,
+                        msg: 'Id not exist'
+                    });
+                }
+
+                break;
+            case 'products':
+                model = await Product.findById(id);
+                if(!model) {
+                    return res.status(400).json({
+                        ok: false,
+                        msg: 'Id not exist'
+                    });
+                }
+                break;
+            default:
+                res.status(500).json({
+                    ok: false,
+                    msg: 'Forgot implemet collection'
+                })
+        }
+
+        // Clean prev files
+        if(model.img) {
+            const pathImg = path.join(__dirname, `../uploads/${collection}`, model.img);
+            if(fs.existsSync(pathImg)) {
+                return res.sendFile(pathImg)
+            }
+        }else{
+            const pathImg = path.join(__dirname, '../assets/', 'no-image.jpg');
+            if(fs.existsSync(pathImg)) {
+                return res.sendFile(pathImg)
+            } 
+        }
+
+        res.status(500).json({
+            ok: false,
+            msg: 'Something goes wrong'
+        })
+
+    }catch(err) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Unexpected error'
+        })
+    }
+}
+
 module.exports = {
     loadFile,
-    updateImage
+    updateImage,
+    showImage
 }
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,17 +1,24 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { loadFile, updateImage } = require('../controllers/uploads');
+const { loadFile, updateImage, showImage } = require('../controllers/uploads');
 const { allowedCollections } = require('../helpers');
 const { validateFields, validateFile } = require('../middlewares');
 
 const router = Router();
 
 router.post('/', validateFile, loadFile);
+
 router.put('/:collection/:id', [
     validateFile,
     check('id', 'Invalid Id').isMongoId(),
     check('collection').custom(c => allowedCollections(c, ['users','products'])),
     validateFields
 ], updateImage);
+
+router.get('/:collection/:id', [
+    check('id', 'Invalid Id').isMongoId(),
+    check('collection').custom(c => allowedCollections(c, ['users','products'])),
+    validateFields
+], showImage);
 
 module.exports = router;
